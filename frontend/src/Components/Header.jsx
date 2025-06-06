d1f9572af98a14399facb075d95d43407735b1f0
Syeda Zainab afridi <syedazainabafridi@gmail.com>
2025-06-07 03:23:23 +0530

    modal added for user login

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,8 +1,31 @@
+import { RiShieldUserFill } from "react-icons/ri";
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 const Header = () => {
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     return (
         <>
         <div className="header">
-        <h3>Task Management System</h3>
+        <h3>Task Management System</h3> <RiShieldUserFill id="usrIcn" onClick={handleShow}/>
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
         </div>
         </>
     )
