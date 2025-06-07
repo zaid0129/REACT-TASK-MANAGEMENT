e3d146e944170a2ef0c182b2d8a08a4bb891e7df
Syeda Zainab afridi <syedazainabafridi@gmail.com>
2025-06-08 03:13:48 +0530

    creating layout odf dashboard

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 const Dashboard=()=>{
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const toggleShow = () => setShow((s) => !s);
     return(
         <>
 
